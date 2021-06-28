171c7b4d53fb1c4502f4c30ded6a32afeb8fec03
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2021-06-28 12:53:50 +0530

    fullgaps patch added

--- config.h
+++ config.h
@@ -1,22 +1,23 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
-static const unsigned int gappx     = 4;        /* gap pixel between windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 14;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12" };
 static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#222222"; /* background color */
-static const char col_gray2[]       = "#745c00"; /* inactive window border color */
+static const char col_gray2[]       = "#2b2f33"; /* inactive window border color */
 static const char col_gray3[]       = "#d0d0d0"; /* font color */
 static const char col_gray4[]       = "#f0f0f0"; /* current tag and current window font color */
-static const char col_cyan[]        = "#462d81"; /* top bar second color and active window border color */
+static const char col_cyan[]        = "#2d72ae"; /* top bar second color */
+static const char act_win_bord[]    = "#307ec2"; /* active window border color */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan, act_win_bord  },
 };
 
 /* tagging */
@@ -87,6 +88,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	{ MODKEY,                       XK_Left,   shiftview,      {.i = -1 } },
 	{ MODKEY,                       XK_Right,  shiftview,      {.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
