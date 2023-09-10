df41ea51e1a1fedbb31338628c0092a790f421df
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-09-11 01:07:56 +0530

    hp switch in dmenucmd
    pass high priority app list to dmenu_run

--- config.h
+++ config.h
@@ -57,9 +57,12 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
+/* high priority apps for dmenu */
+static const char dmenuhpapps[] = "librewolf,brave-bin,emacsc,signal-desktop";
+
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-hp", dmenuhpapps, NULL };
 static const char *termcmd[]    = { "alacritty", NULL }; /* Default terminal emulator for dwm */
 static const char *rangercmd[]  = { "alacritty", "-e", "ranger", NULL};
 static const char *brupcmd[]    = { "light", "-A", "5", NULL};
