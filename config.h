76044e6114fa5b54263d646a39c2d19192d07c21
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2024-09-04 00:48:46 +0530

    dmenu invoke: update highpriority apps
    add logseq to highpriority apps

--- config.h
+++ config.h
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* high priority apps for dmenu */
-static const char dmenuhpapps[] = "librewolf-bin,brave-bin,torbrowser-launcher,signal-desktop,virt-manager";
+static const char dmenuhpapps[] = "librewolf-bin,brave-bin,torbrowser-launcher,signal-desktop,virt-manager,logseq";
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
