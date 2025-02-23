43b5260924a5841222d92e60f3a3d56ce4a8b30a
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2025-02-23 14:07:39 +0530

    theme change

--- config.h
+++ config.h
@@ -1,19 +1,19 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 4;        /* border pixel of windows */
-static const unsigned int gappx     = 12;       /* gap pixel between windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 8;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=15" };
-static const char dmenufont[]       = "monospace:size=15";
+static const char *fonts[]          = { "MartianMono Nerd Font:size=9" };
+static const char dmenufont[]       = "MartianMono Nerd Font:size=9";
 static const char col_gray1[]       = "#222222"; /* background color */
 static const char col_gray2[]       = "#2b2f33"; /* inactive window border color */
 static const char col_gray3[]       = "#d0d0d0"; /* font color */
 static const char col_gray4[]       = "#f0f0f0"; /* current tag and current window font color */
-static const char col_cyan[]        = "#524f95"; /* top bar second color */
-static const char act_win_bord[]    = "#454290"; /* active window border color */
+static const char col_cyan[]        = "#bd93f9"; /* top bar second color */
+static const char act_win_bord[]    = "#bd93f9"; /* active window border color */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -21,7 +21,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -46,7 +46,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
@@ -62,11 +62,13 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]    = { "alacritty", NULL }; /* Default terminal emulator for dwm */
 static const char *rangercmd[]  = { "alacritty", "-e", "ranger", NULL};
-static const char *brupcmd[]    = { "light", "-A", "5", NULL};
-static const char *brdowncmd[]  = { "light", "-U", "5", NULL};
-static const char *volupcmd[]   = { "amixer", "-D", "pulse", "sset", "Master", "2%+", NULL};
-static const char *voldowncmd[] = { "amixer", "-D", "pulse", "sset", "Master", "2%-", NULL};
-static const char *volmutecmd[] = { "amixer", "-D", "pulse", "sset", "Master", "0%", NULL};
+static const char *brupcmd[]    = { "xbacklight", "-inc", "5", NULL};
+static const char *brdowncmd[]  = { "xbacklight", "-dec", "5", NULL};
+static const char *kbdbrupcmd[]    = { "kbdbacklight", "-inc", "5", NULL};
+static const char *kbdbrdowncmd[]  = { "kbdbacklight", "-dec", "5", NULL};
+static const char *volupcmd[]   = { "amixer", "set", "Master", "2%+", NULL};
+static const char *voldowncmd[] = { "amixer", "set", "Master", "2%-", NULL};
+static const char *volmutecmd[] = { "amixer", "set", "Master", "0%", NULL};
 static const char *sscmd[]      = { "scrot", "-fs", NULL };
 
 #include "shiftview.c"
@@ -75,6 +77,8 @@ static Key keys[] = {
     /* modifier             key                         function        argument */
 	{ 0,                    XF86XK_MonBrightnessUp,     spawn,          {.v = brupcmd } },
 	{ 0,                    XF86XK_MonBrightnessDown,   spawn,          {.v = brdowncmd } },
+  { 0,                    XF86XK_KbdBrightnessUp,     spawn,          {.v = kbdbrupcmd } },
+	{ 0,                    XF86XK_KbdBrightnessDown,   spawn,          {.v = kbdbrdowncmd } },
 	{ 0,                    XF86XK_AudioRaiseVolume,    spawn,          {.v = volupcmd } },
 	{ 0,                    XF86XK_AudioLowerVolume,    spawn,          {.v = voldowncmd } },
 	{ 0,                    XF86XK_AudioMute,           spawn,          {.v = volmutecmd } },
@@ -114,6 +118,7 @@ static Key keys[] = {
 	TAGKEYS(                XK_4,                                        3)
 	TAGKEYS(                XK_5,                                        4)
 	TAGKEYS(                XK_6,                                        5)
+	TAGKEYS(                XK_7,                                        6)
 	{ MODKEY|ShiftMask,     XK_q,                       quit,           {0} },
 };
 
