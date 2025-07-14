ba59103cba276ee23ad0fc2888a81457e40d4668
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2025-07-14 12:00:34 +0530

    update font

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 8;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "MartianMono Nerd Font:size=9" };
-static const char dmenufont[]       = "MartianMono Nerd Font:size=9";
+static const char *fonts[]          = { "MartianMono Nerd Font:size=11" };
+static const char dmenufont[]       = "MartianMono Nerd Font:size=11";
 static const char col_gray1[]       = "#222222"; /* background color */
 static const char col_gray2[]       = "#2b2f33"; /* inactive window border color */
 static const char col_gray3[]       = "#d0d0d0"; /* font color */
